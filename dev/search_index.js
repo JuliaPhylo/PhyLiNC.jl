ee0dd3856ce8549eab465edbc4708db6e2225b76
Documenter.jl <documenter@juliadocs.github.io>
2024-09-12 18:23:39 +0000

    build based on c787023

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhyLiNC","category":"page"},{"location":"#PhyLiNC","page":"Home","title":"PhyLiNC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhyLiNC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhyLiNC]","category":"page"}]
+}
