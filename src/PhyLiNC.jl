93250e972810e3fe33dd5db7281898da8c74b477
Cecile Ane <cecileane@users.noreply.github.com>
2024-09-13 23:25:09 -0400

    bare start

--- src/PhyLiNC.jl
+++ src/PhyLiNC.jl
@@ -1,5 +1,36 @@
 module PhyLiNC
 
-# Write your package code here.
+using BioSequences
+using BioSymbols
+using CSV
+using DataFrames # innerjoin new in v0.21
+using Distributions #for RateVariationAcrossSites
+using FASTX
+using GLM
+using LinearAlgebra: mul!, lmul!
+using NLopt
+using PhyloNetworks
+using PhyloNetworks: Edge, Node, TopologyConstraint, fliphybrid!,
+        deletehybridedge!, addhybridedge!
+using PhyloTraits
+using PhyloTraits: StatisticalSubstitutionModel, P!, update_logtrans,
+        discrete_corelikelihood_trait!, discrete_corelikelihood!,
+        discrete_backwardlikelihood_trait!
+using Random
+using StaticArrays
+using StatsAPI: StatsAPI, loglikelihood
+using StatsBase
+using StatsFuns # logsumexp, logaddexp, log2π, various cdf
+using StatsModels # re-exported by GLM. for ModelFrame ModelMatrix Formula etc
+
+const PN = PhyloNetworks
+const SSM = PhyloTraits.StatisticalSubstitutionModel
+
+import Base: show
+import StatsAPI: fit!
+
+export phyLiNC
+
+include("phyLiNCoptimization.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,29 @@ using PhyLiNC
 using Test
 using Aqua
 
-@testset "PhyLiNC.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(PhyLiNC)
-    end
-    # Write your tests here.
+using BioSymbols
+using CSV
+using DataFrames
+using Distributed
+using PhyloNetworks
+using PhyloTraits
+using PhyloTraits: StatisticalSubstitutionModel, discrete_corelikelihood!
+using Random
+using StaticArrays # for rate substitution matrices
+using Statistics
+using StatsBase # for aic etc., stderr
+
+const PN = PhyloNetworks
+
+@testset "PhyLiNC code quality (Aqua.jl)" begin
+    Test.detect_ambiguities(PhyLiNC)
+    Aqua.test_all(
+        PhyLiNC;
+        ambiguities = (broken=false),
+        persistent_tasks = false,
+    )
+end
+
+@testset "PhyLiNC" begin
+    include("test_phyLiNCoptimization.jl")
 end
